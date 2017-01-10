15e6b26f2427cfc3e0547fef24127abaf13c706c
Cas Eliëns <cas.eliens@gmail.com>
2017-01-10 09:51:00 +0100

    Store message room ID as string

--- lib/lita/adapters/discord_oauth.rb
+++ lib/lita/adapters/discord_oauth.rb
@@ -20,15 +20,16 @@ module Lita
             message = event.message
             author_id = message.author.id.to_s
 
-            Lita.logger.debug('Message: ' + message.content)
-            Lita.logger.debug('Author ID: ' + author_id)
+            Lita.logger.debug('Received message from ' + author_id + ': ' + message.content)
 
             user = Lita::User.find_by_id(author_id)
             user = Lita::User.create(author_id) unless user
 
             Lita.logger.debug('User ID: ' + user.id)
 
-            channel = event.channel.id
+            channel = event.channel.id.to_s
+
+            Lita.logger.debug('Channel ID: ' + channel)
 
             source = Lita::Source.new(user: user, room: channel)
             msg = Lita::Message.new(robot, message.content, source)
